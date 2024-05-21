282d0855b5db69a6fbc07ea5d69223247f029204
Ribesh <ribeshpokharel@gmail.com>
2024-05-21 23:22:47 +1000

    3rd commit

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    [SerializeField]
+    private float enemyInterval = 3.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spwanEnemy(enemyInterval, enemyPrefab));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator spwanEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-7f, 7), Random.Range(7f, 7f), 0), Quaternion.identity);
+        StartCoroutine(spwanEnemy(interval, enemy));
     }
 }
